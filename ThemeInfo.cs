9d869f459c9df1da666ea4e7196f9eb0b6cbc3b5
Shaun Walker <shaun.walker@siliqon.com>
2020-05-24 20:41:12 -0400

    Add project files.

---
+++ ThemeInfo.cs
@@ -0,0 +1,13 @@
+﻿using Oqtane.Themes;
+
+namespace Oqtane.Theme.Cerulean
+{
+    public class ThemeInfo : ITheme
+    {
+        public Models.Theme Theme => new Models.Theme
+        {
+            Name = "Bootswatch Cerulean Theme",
+            Version = "1.0.0"
+        };
+    }
+}
---
+++ debug.cmd
@@ -0,0 +1,3 @@
+XCOPY "bin\Debug\netstandard2.1\Oqtane.Theme.Cerulean.Oqtane.dll" "..\oqtane.framework\Oqtane.Server\bin\Debug\netcoreapp3.1\" /Y
+XCOPY "bin\Debug\netstandard2.1\Oqtane.Theme.Cerulean.Oqtane.pdb" "..\oqtane.framework\Oqtane.Server\bin\Debug\netcoreapp3.1\" /Y
+XCOPY "wwwroot\Themes\Oqtane.Theme.Cerulean\*" "..\oqtane.framework\Oqtane.Server\wwwroot\Themes\Oqtane.Theme.Cerulean\" /Y /S /I
