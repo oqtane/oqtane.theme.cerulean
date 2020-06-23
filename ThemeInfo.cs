3104f18350ea62d459c4427b2cafeae14703bd57
Shaun Walker <shaun.walker@siliqon.com>
2020-06-23 11:38:12 -0400

    upgrade to 1.0.1 release

--- ThemeInfo.cs
+++ ThemeInfo.cs
@@ -7,7 +7,7 @@ namespace Oqtane.Theme.Cerulean
         public Models.Theme Theme => new Models.Theme
         {
             Name = "Bootswatch Cerulean Theme",
-            Version = "1.0.0"
+            Version = "1.0.1"
         };
     }
 }
